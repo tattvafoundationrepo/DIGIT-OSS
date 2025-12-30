74e4b29b169a57a7b69fbe26d215a55605b8ada0
devendra-yt <devendra@tattvafoundation.org>
2025-12-30 16:52:14 +0530

    Update EmailAttachment.java
    add for attachement file

--- core-services/egov-notification-mail/src/main/java/org/egov/web/notification/mail/consumer/contract/EmailAttachment.java
+++ core-services/egov-notification-mail/src/main/java/org/egov/web/notification/mail/consumer/contract/EmailAttachment.java
@@ -1,1 +1,22 @@
 
+package org.egov.web.notification.mail.consumer.contract;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailAttachment {
+    
+    @JsonProperty("name")
+    private String name; 
+    
+    @JsonProperty("mimeType")
+    private String mimeType; 
+    
+    @JsonProperty("data")
+    private String data;
+}
